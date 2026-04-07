1fbf7c56bc1235b092e6a926bd7c86cc9c59a817
26FGD01-FatimaGonzalez <gonzalezfatima16db@gmail.com>
2026-04-07 10:53:09 -0600

    Add files via upload

---
+++ DIASdelaSemana.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("=========Dias de la semana=========");
+        Console.WriteLine("Digite un numero del 1 al 7 para mostrar el dia de la semana");
+        int dia;
+        //solicitar el dato
+        dia = Convert.ToInt32(Console.ReadLine());
+        switch (dia)
+        { 
+            case 1:
+                Console.WriteLine("Lunes");
+                break;
+            case 2:
+                Console.WriteLine("Martes");
+                break;
+            case 3:
+                Console.WriteLine("Miercoles");
+                break;
+            case 4:
+                Console.WriteLine("Jueves");
+                break;
+            case 5:
+                Console.WriteLine("Viernes");
+                break;
+            case 6:
+                Console.WriteLine("Sabado");
+                break;
+            case 7:
+                Console.WriteLine("Domingo");
+                break;
+            default:
+                Console.WriteLine("Digite un valor entre el 1 al 7");
+                break;
+        }
+    }
+}
